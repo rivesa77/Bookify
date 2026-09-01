3e97a57e116cdd5937cc95ba5dac5b41f7f62bb5
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-01 19:07:43 +0200

    Añadido listado y propiedades de domainEvents a Entity

--- Bookify.Domain/Abstractions/Entity.cs
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -4,11 +4,28 @@
 
     public abstract class Entity
     {
+        private readonly List<IDomainEvent> domainEvents = [];
+
         protected Entity(Guid id)
         {
             Id = id;
         }
 
         public Guid Id { get; init; }
+
+        public IReadOnlyList<IDomainEvent> GetDomainEvents()
+        {
+            return [.. domainEvents];
+        }
+
+        public void ClearDomainEvent()
+        {
+            domainEvents.Clear();
+        }
+
+        protected void RaiseDomainEvent(IDomainEvent domainEvent)
+        {
+            domainEvents.Add(domainEvent);
+        }
     }
 }
