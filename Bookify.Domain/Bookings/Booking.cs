1edf5ea6fe735aca6dbdcac8b8cbe00a8ff70266
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-01 21:02:44 +0200

    Creada entidad Booking y records

---
+++ Bookify.Domain/Bookings/Booking.cs
@@ -0,0 +1,73 @@
+﻿namespace Bookify.Domain.Bookings
+{
+    using Bookify.Domain.Abstractions;
+    using Bookify.Domain.Commons;
+
+    public sealed class Booking : Entity
+    {
+        private Booking(
+            Guid id,
+            Guid apartmentId,
+            Guid userId,
+            DateRange duration,
+            Money priceForPeriod,
+            Money cleaningFee,
+            Money amenitiesUpChange,
+            Money totalPrice,
+            BookingStatus status,
+            DateTime createdOnUtc,
+            DateTime? confirmedOnUtc,
+            DateTime? rejectedOnUtc,
+            DateTime? completedOnUtc,
+            DateTime? cancelledOnUtc) : base(id)
+        {
+            ApartmentId = apartmentId;
+            UserId = userId;
+            Duration = duration;
+            PriceForPeriod = priceForPeriod;
+            CleaningFee = cleaningFee;
+            AmenitiesUpChange = amenitiesUpChange;
+            TotalPrice = totalPrice;
+            Status = status;
+            CreatedOnUtc = createdOnUtc;
+            ConfirmedOnUtc = confirmedOnUtc;
+            RejectedOnUtc = rejectedOnUtc;
+            CompletedOnUtc = completedOnUtc;
+            CancelledOnUtc = cancelledOnUtc;
+        }
+
+        public Guid ApartmentId { get; private set; }
+
+        public Guid UserId { get; private set; }
+
+        public DateRange Duration { get; private set; }
+
+        public Money PriceForPeriod { get; private set; }
+
+        public Money CleaningFee { get; private set; }
+
+        public Money AmenitiesUpChange { get; private set; }
+
+        public Money TotalPrice { get; private set; }
+
+        public BookingStatus Status { get; private set; }
+
+        public DateTime CreatedOnUtc { get; private set; }
+
+        public DateTime? ConfirmedOnUtc { get; private set; }
+
+        public DateTime? RejectedOnUtc { get; private set; }
+
+        public DateTime? CompletedOnUtc { get; private set; }
+
+        public DateTime? CancelledOnUtc { get; private set; }
+
+        public static Booking Reserve(
+            Guid apartmentId,
+            Guid userId,
+            DateRange duration,
+            DateTime utcNow)
+        {
+        }
+    }
+}
---
+++ Bookify.Domain/Bookings/DateRange.cs
@@ -0,0 +1,31 @@
+﻿namespace Bookify.Domain.Bookings
+{
+    public record DateRange
+    {
+        private DateRange()
+        {
+        }
+
+        public DateOnly Start { get; init; }
+
+        public DateOnly End { get; init; }
+
+        public int LengthInDays => End.DayNumber - Start.DayNumber;
+
+        public static DateRange Create(DateOnly start, DateOnly end)
+        {
+            if (start < end)
+            {
+                throw new ApplicationException("End date precedes start date.");
+            }
+
+            DateRange dateRange = new()
+            {
+                Start = start,
+                End = end
+            };
+
+            return dateRange;
+        }
+    }
+}
