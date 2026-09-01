224abb5c50d8c5e98d3789c34b38c11cffffd46d
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-01 19:58:10 +0200

    Creadas interfaces de Repository para Apartment y User

---
+++ Bookify.Domain/Apartments/IApartmentRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookify.Domain.Apartments
+{
+    using System;
+
+    public interface IApartmentRepository
+    {
+        Task<Apartment?> GetByIdAsync(Guid Id, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Bookify.Domain/Users/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookify.Domain.Users
+{
+    using System;
+
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+        void Add(User user);
+    }
+}
