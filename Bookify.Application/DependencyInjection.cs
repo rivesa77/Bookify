a0ba416a4b5df5056f0a121fe2576e3e06bca5f8
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-03 17:40:54 +0200

    Añadido Extension con Denpendencias y añadida referencia a proyecto Domain

---
+++ Bookify.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿namespace Bookify.Application
+{
+    using Bookify.Domain.Bookings;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+
+            services.AddTransient<PricingServices>();
+
+            return services;
+        }
+    }
+}
