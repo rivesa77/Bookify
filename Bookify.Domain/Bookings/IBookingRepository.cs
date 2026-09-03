a9f38a6a5769e22c40faf39017d48f832501134f
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-03 13:16:32 +0200

    Creado IBookingRepository

---
+++ Bookify.Domain/Bookings/IBookingRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Bookify.Domain.Bookings
+{
+    using System;
+    using Bookify.Domain.Apartments;
+
+    public interface IBookingRepository
+    {
+        Task<Booking?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+        Task<bool> IsOverlappingAsync(
+            Apartment apartment,
+            DateRange duration,
+            CancellationToken cancellationToken = default);
+
+        void Add(Booking booking);
+    }
+}
