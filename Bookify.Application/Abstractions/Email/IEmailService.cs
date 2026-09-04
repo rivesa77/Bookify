4beeee0d624ccec86a19f39bd1e5380d1da49fa1
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-04 11:08:32 +0200

    Creado interface para IEmailService

---
+++ Bookify.Application/Abstractions/Email/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookify.Application.Abstractions.Email
+{
+    using Bookify.Domain.Users;
+
+    public interface IEmailService
+    {
+        Task SendAsync(
+            Email recipient,
+            string subject,
+            string body);
+    }
+}
