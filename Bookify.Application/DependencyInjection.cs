260856fbb6e1a413e695b0ec9ec8107fafeb5afd
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-07 13:42:32 +0200

    Añadidas dependencias del FluentValidator

--- Bookify.Application/DependencyInjection.cs
+++ Bookify.Application/DependencyInjection.cs
@@ -2,6 +2,7 @@
 {
     using Bookify.Application.Abstractions.Behaviors;
     using Bookify.Domain.Bookings;
+    using FluentValidation;
     using Microsoft.Extensions.DependencyInjection;
 
     public static class DependencyInjection
@@ -12,7 +13,10 @@
             {
                 configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
                 configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+                configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
             });
+
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
 
             services.AddTransient<PricingServices>();
 
