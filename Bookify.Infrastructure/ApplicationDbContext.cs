3c76d9a48b4dd27bba6749bb3142396f910019ca
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-07 20:03:15 +0200

    Creado ApplicationDbContext y configuracion dependencias para postgre

---
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookify.Infrastructure
+{
+    using Bookify.Domain.Abstractions;
+    using Microsoft.EntityFrameworkCore;
+
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
--- Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -4,6 +4,7 @@
     using Bookify.Application.Abstractions.Email;
     using Bookify.Infrastructure.Clock;
     using Bookify.Infrastructure.Email;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
 
@@ -14,6 +15,15 @@
             services
                 .AddTransient<IDateTimeProvider, DateTimeProvider>()
                 .AddTransient<IEmailService, EmailService>();
+
+            string connectionString = configuration.GetConnectionString("DataBase") ??
+                throw new ArgumentNullException(nameof(configuration));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                // UseSnakeCaseNamingConvention automatically map C# class and property names to snake_case in the database.
+                options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+            });
 
             return services;
         }
