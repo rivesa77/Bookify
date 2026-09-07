e3ead7f70b38a503b0e00dac9463886f4bc8cc89
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-07 13:41:58 +0200

    Creada clase ReserveBookingCommandValidator para validar la reserva

---
+++ Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace Bookify.Application.Bookings.ReserveBooking
+{
+    using FluentValidation;
+
+    public class ReserveBookingCommandValidator : AbstractValidator<ReserveBookingCommand>
+    {
+        public ReserveBookingCommandValidator()
+        {
+            RuleFor(c => c.UserId).NotEmpty();
+            RuleFor(c => c.ApartmentId).NotEmpty();
+            RuleFor(c => c.StartDate).LessThan(c => c.EndDate);
+        }
+    }
+}
