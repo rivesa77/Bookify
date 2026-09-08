4dee9ce5decc8cae50d04325e348c4de4c120b7e
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-08 10:43:25 +0200

    Sobrecarga OnModelCreating en ApplicationDbContext para aplicar configurationFromAssembly

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -9,5 +9,12 @@
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
