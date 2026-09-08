3447be6dde5b70e443d25becc4781279d9dfbe08
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-08 11:51:18 +0200

    Creada clase concreta de SqlConnectionFactory

---
+++ Bookify.Infrastructure/Data/SqlConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace Bookify.Infrastructure.Data
+{
+    using System.Data;
+    using Bookify.Application.Abstractions.Data;
+    using Npgsql;
+
+    internal sealed class SqlConnectionFactory : ISqlConnectionFactory
+    {
+        private readonly string connectionString;
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            NpgsqlConnection connection = new(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
