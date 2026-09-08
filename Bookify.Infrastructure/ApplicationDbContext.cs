8f35499f9be4df4f0f9ad8b91616003122d0c11e
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-08 12:48:22 +0200

    Añadido metodo de publicacion de eventos de dominio, (sin terminar)

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
 ﻿namespace Bookify.Infrastructure
 {
     using Bookify.Domain.Abstractions;
+    using MediatR;
     using Microsoft.EntityFrameworkCore;
 
     public sealed class ApplicationDbContext : DbContext, IUnitOfWork
     {
-        public ApplicationDbContext(DbContextOptions options)
+        private readonly IPublisher publisher;
+
+        public ApplicationDbContext(DbContextOptions options, IPublisher publisher)
             : base(options)
         {
+            this.publisher = publisher;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -15,6 +19,35 @@
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            int result = await base.SaveChangesAsync(cancellationToken);
+
+            await PublishDomainEventAsync();
+
+            return result;
+        }
+
+        private async Task PublishDomainEventAsync()
+        {
+            IReadOnlyList<IDomainEvent> domainEvents = [.. ChangeTracker
+                .Entries<Entity>()
+                .Select(entry => entry.Entity)
+                .SelectMany(entity =>
+                {
+                    IReadOnlyList<IDomainEvent> domainEvents = entity.GetDomainEvents();
+
+                    entity.ClearDomainEvent();
+
+                    return domainEvents;
+                })];
+
+            foreach (IDomainEvent domainEvent in domainEvents)
+            {
+                await publisher.Publish(domainEvent);
+            }
         }
     }
 }
