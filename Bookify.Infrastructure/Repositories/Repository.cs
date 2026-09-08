7822eff8983c666ad0d1088771a46a6cdf63a979
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-08 10:56:49 +0200

    Creada clase abstracta Repository en infraestructura

---
+++ Bookify.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,28 @@
+﻿namespace Bookify.Infrastructure.Repositories
+{
+    using Bookify.Domain.Abstractions;
+    using Microsoft.EntityFrameworkCore;
+
+    internal abstract class Repository<TEntity>
+        where TEntity : Entity
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        protected Repository(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<TEntity?> GetEntityAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await applicationDbContext
+                .Set<TEntity>()
+                .FirstOrDefaultAsync(user => user.Id == id, cancellationToken);
+        }
+
+        public void Add(TEntity entity)
+        {
+            applicationDbContext.Add(entity);
+        }
+    }
+}
