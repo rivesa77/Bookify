e028ffa0e859b325bd2619c75325d137923395a7
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-08 18:15:34 +0200

    Añadidas referencias e inyecciones de dependencias en el program

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -1,8 +1,14 @@
+using Bookify.Application;
+using Bookify.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
