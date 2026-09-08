381768bdf62098dd9244f32c13f57c0ae4173701
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-08 11:53:59 +0200

    Clase para formatear solo las fechas en postgre

---
+++ Bookify.Infrastructure/Data/DateOnlyTypeHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Bookify.Infrastructure.Data
+{
+    using System;
+    using System.Data;
+    using Dapper;
+
+    internal sealed class DateOnlyTypeHandler : SqlMapper.TypeHandler<DateOnly>
+    {
+        public override DateOnly Parse(object value)
+        {
+            return DateOnly.FromDateTime((DateTime)value);
+        }
+
+        public override void SetValue(IDbDataParameter parameter, DateOnly value)
+        {
+            parameter.DbType = DbType.Date;
+            parameter.Value = value;
+        }
+    }
+}
