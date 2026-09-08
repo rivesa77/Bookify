0c15293760d18214b4f1441339d7d2b8bc70a494
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-08 18:53:55 +0200

    Creado controladores de Apartment y booking

---
+++ Bookify.Api/Controllers/Apartments/ApartmentsController.cs
@@ -0,0 +1,32 @@
+﻿namespace Bookify.Api.Controllers.Apartment
+{
+    using Bookify.Application.Apartments.SearchApartments;
+    using Bookify.Domain.Abstractions;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/apartments")]
+    public class ApartmentsController : ControllerBase
+    {
+        private readonly ISender sender;
+
+        public ApartmentsController(ISender sender)
+        {
+            this.sender = sender;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchApartments(
+            DateOnly starDate,
+            DateOnly endDate,
+            CancellationToken cancellationToken)
+        {
+            SearchApartmentsQuery query = new(starDate, endDate);
+
+            Result<IReadOnlyList<ApartmentResponse>> result = await sender.Send(query, cancellationToken);
+
+            return Ok(result.Value);
+        }
+    }
+}
