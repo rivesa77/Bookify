71b1db7baa88a4009094a5b6805cf9bda11ce1d0
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-09 12:28:37 +0200

    Creada la migracion inicial

---
+++ Bookify.Infrastructure/Migrations/20260909102321_Initial_Database.cs
@@ -0,0 +1,177 @@
+﻿#nullable disable
+
+namespace Bookify.Infrastructure.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc />
+    public partial class Initial_Database : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Apartments",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: true),
+                    address_country = table.Column<string>(type: "text", nullable: true),
+                    address_state = table.Column<string>(type: "text", nullable: true),
+                    address_zip_code = table.Column<string>(type: "text", nullable: true),
+                    address_city = table.Column<string>(type: "text", nullable: true),
+                    address_street = table.Column<string>(type: "text", nullable: true),
+                    price_amount = table.Column<decimal>(type: "numeric", nullable: true),
+                    price_currency = table.Column<string>(type: "text", nullable: true),
+                    cleaning_fee_amount_amount = table.Column<decimal>(type: "numeric", nullable: true),
+                    cleaning_fee_amount_currency = table.Column<string>(type: "text", nullable: true),
+                    last_booked_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    amenities = table.Column<int[]>(type: "integer[]", nullable: true),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_apartments", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    first_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    last_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    email = table.Column<string>(type: "character varying(400)", maxLength: 400, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    apartment_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    duration_start = table.Column<DateOnly>(type: "date", nullable: true),
+                    duration_end = table.Column<DateOnly>(type: "date", nullable: true),
+                    price_for_period_amount = table.Column<decimal>(type: "numeric", nullable: true),
+                    price_for_period_currency = table.Column<string>(type: "text", nullable: true),
+                    cleaning_fee_amount = table.Column<decimal>(type: "numeric", nullable: true),
+                    cleaning_fee_currency = table.Column<string>(type: "text", nullable: true),
+                    amenities_up_change_amount = table.Column<decimal>(type: "numeric", nullable: true),
+                    amenities_up_change_currency = table.Column<string>(type: "text", nullable: true),
+                    total_price_amount = table.Column<decimal>(type: "numeric", nullable: true),
+                    total_price_currency = table.Column<string>(type: "text", nullable: true),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    created_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    confirmed_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    rejected_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    completed_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    cancelled_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_bookings", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_bookings_apartments_apartment_id",
+                        column: x => x.apartment_id,
+                        principalTable: "Apartments",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_bookings_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    apartment_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    booking_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    rating = table.Column<int>(type: "integer", nullable: true),
+                    comment = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    created_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_reviews", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_reviews_apartments_apartment_id",
+                        column: x => x.apartment_id,
+                        principalTable: "Apartments",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_reviews_bookings_booking_id",
+                        column: x => x.booking_id,
+                        principalTable: "Bookings",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_reviews_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bookings_apartment_id",
+                table: "Bookings",
+                column: "apartment_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bookings_user_id",
+                table: "Bookings",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_reviews_apartment_id",
+                table: "Reviews",
+                column: "apartment_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_reviews_booking_id",
+                table: "Reviews",
+                column: "booking_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_reviews_user_id",
+                table: "Reviews",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email",
+                table: "Users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Apartments");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
