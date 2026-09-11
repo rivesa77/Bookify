5da84f4a4c57abaaf291de4a4c985e07cb73e9af
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-11 12:51:49 +0200

    Creados test para comprobar nombres de las clases segun tipo interface que implementan

---
+++ Tests/Bookify.ArchitectureTest/Application/ApplicationTests.cs
@@ -0,0 +1,72 @@
+﻿namespace Bookify.ArchitectureTest.Application
+{
+    using Bookify.Application.Abstractions.Messaging;
+    using FluentAssertions;
+    using FluentValidation;
+    using NetArchTest.Rules;
+
+    [TestClass]
+    [TestCategory("Architecture")]
+    public class ApplicationTests : BaseTest
+    {
+        [TestMethod]
+        public void CommandHandlerWithReflection_Should_HaveNameEndingWith_CommandHandler()
+        {
+            var invalidHandlers = ApplicationAssembly
+                .GetTypes()
+                .Where(type => type.IsClass && !type.IsAbstract)
+                .Where(type => type.GetInterfaces().Any(@interface =>
+                    @interface.IsGenericType &&
+                    (
+                        @interface.GetGenericTypeDefinition() == typeof(ICommandHandler<>) ||
+                        @interface.GetGenericTypeDefinition() == typeof(ICommandHandler<,>)
+                    )))
+                .Where(type => !type.Name.Split('`')[0]
+                    .EndsWith("CommandHandler", StringComparison.Ordinal))
+                .Select(type => type.FullName)
+                .ToArray();
+
+            invalidHandlers.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void CommandHandler_Should_HaveNameEndingWith_CommandHandler()
+        {
+            var result = Types.InAssembly(ApplicationAssembly)
+                .That()
+                .ImplementInterface(typeof(ICommandHandler<>))
+                .Or()
+                .ImplementInterface(typeof(ICommandHandler<,>))
+                .Should().HaveNameEndingWith("CommandHandler")
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void QueryHandler_Should_HaveNameEndingWith_QueryHandler()
+        {
+            var result = Types.InAssembly(ApplicationAssembly)
+                .That()
+                .ImplementInterface(typeof(IQueryHandler<,>))
+                .Should()
+                .HaveNameEndingWith("QueryHandler")
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Validator_Should_HaveNameEndingWith_Validator()
+        {
+            var result = Types.InAssembly(ApplicationAssembly)
+                .That()
+                .Inherit(typeof(AbstractValidator<>))
+                .Should()
+                .HaveNameEndingWith("Validator")
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+    }
+}
---
+++ Tests/Bookify.ArchitectureTest/Domain/DomainTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Bookify.ArchitectureTest.Domain
+{
+    using Bookify.Domain.Abstractions;
+    using FluentAssertions;
+    using NetArchTest.Rules;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    [TestCategory("Architecture")]
+    public class DomainTests : BaseTest
+    {
+        [TestMethod]
+        public void DomainEvent_Should_Have_DomainEventPostfix()
+        {
+            var result = Types.InAssembly(DomainAssembly)
+                .That()
+                .ImplementInterface(typeof(IDomainEvent))
+                .Should().HaveNameEndingWith("DomainEvent")
+                .GetResult();
+
+            result.IsSuccessful
+                .Should()
+                .BeTrue();
+        }
+    }
+}
