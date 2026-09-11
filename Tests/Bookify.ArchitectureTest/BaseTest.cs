690c338caa602c053875151a2af8f7d2275b742c
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-11 11:12:17 +0200

    Creados test para comprobar que las dependencias entre proyectos son correctas

---
+++ Tests/Bookify.ArchitectureTest/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Bookify.ArchitectureTest
+{
+    using System.Reflection;
+    using Bookify.Application.Abstractions.Messaging;
+    using Bookify.Domain.Abstractions;
+    using Bookify.Infrastructure;
+
+    public class BaseTest
+    {
+        protected static Assembly DomainAssembly => typeof(Entity).Assembly;
+
+        protected static Assembly ApplicationAssembly => typeof(IBaseCommand).Assembly;
+
+        protected static Assembly InfrastructureAssembly => typeof(ApplicationDbContext).Assembly;
+    }
+}
---
+++ Tests/Bookify.ArchitectureTest/LayerTests.cs
@@ -0,0 +1,67 @@
+﻿namespace Bookify.ArchitectureTest
+{
+    using FluentAssertions;
+    using NetArchTest.Rules;
+
+    [TestClass]
+    [TestCategory("Architecture")]
+    public class LayerTests : BaseTest
+    {
+        [TestMethod]
+        public void DomainLayer_Should_NotHaveDependencyOn_ApplicationLayer()
+        {
+            // Arrange
+            Types domainAssemblies = Types.InAssembly(DomainAssembly);
+
+            // Act
+            TestResult result = domainAssemblies
+                .Should()
+                .NotHaveDependencyOn(ApplicationAssembly.GetName().Name)
+                .GetResult();
+
+            // Assert
+            result
+                .IsSuccessful
+                .Should()
+                .BeTrue();
+        }
+
+        [TestMethod]
+        public void DomainLayer_Should_NotHaveDependencyOn_InfrastructureLayer()
+        {
+            // Arrange
+            Types domainAssemblies = Types.InAssembly(DomainAssembly);
+
+            // Act
+            TestResult result = domainAssemblies
+                .Should()
+                .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+                .GetResult();
+
+            // Assert
+            result
+                .IsSuccessful
+                .Should()
+                .BeTrue();
+        }
+
+        [TestMethod]
+        public void ApplicationLayer_Should_NotHaveDependencyOn_InfrastructureLayer()
+        {
+            // Arrange
+            Types domainAssemblies = Types.InAssembly(ApplicationAssembly);
+
+            // Act
+            TestResult result = domainAssemblies
+                .Should()
+                .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+                .GetResult();
+
+            // Assert
+            result
+                .IsSuccessful
+                .Should()
+                .BeTrue();
+        }
+    }
+}
