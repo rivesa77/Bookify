945c66de1284a4ff5044ef2dc80f04598ff1b5be
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-14 18:51:54 +0200

    Creada clase constantes de apartment

---
+++ Tests/Bookify.TestUtilities/Constants/ApartmentConstants.cs
@@ -0,0 +1,25 @@
+﻿namespace Bookify.TestUtilities.Constants
+{
+    public static class ApartmentConstants
+    {
+        public const string Name = "Apartment Name";
+
+        public const string Description = "Apartment Description";
+
+        public const string Country = "Spain";
+
+        public const string State = "Madrid";
+
+        public const string ZipCode = "28001";
+
+        public const string City = "Vallecas";
+
+        public const string Street = "Street 1";
+
+        public const decimal PriceAmount = 1.5M;
+
+        public const decimal CleaningFeeAmount = 2.35M;
+
+        public const string Currency = "EUR";
+    }
+}
