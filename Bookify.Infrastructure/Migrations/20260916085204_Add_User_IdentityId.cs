3031e8e3b670c447cb2a6e8ad291fc1dcaa92423
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-16 11:03:51 +0200

    Creada migracion con los cambios en User de dominio

---
+++ Bookify.Infrastructure/Migrations/20260916085204_Add_User_IdentityId.cs
@@ -0,0 +1,38 @@
+﻿#nullable disable
+
+namespace Bookify.Infrastructure.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc/>
+    public partial class Add_User_IdentityId : Migration
+    {
+        /// <inheritdoc/>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "identity",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_id",
+                table: "Users",
+                column: "id",
+                unique: true);
+        }
+
+        /// <inheritdoc/>
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_users_id",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "identity",
+                table: "Users");
+        }
+    }
+}
