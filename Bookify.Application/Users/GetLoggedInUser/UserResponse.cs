9eaa1629db991074d5c62a6d1124f17007fca643
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-16 20:39:04 +0200

    Corregido UserResponse

--- Bookify.Application/Users/GetLoggedInUser/UserResponse.cs
+++ Bookify.Application/Users/GetLoggedInUser/UserResponse.cs
@@ -8,6 +8,8 @@
 
         public string Email { get; init; } = string.Empty;
 
-        public string Name { get; init; } = string.Empty;
+        public string FirstName { get; init; } = string.Empty;
+
+        public string LastName { get; init; } = string.Empty;
     }
 }
