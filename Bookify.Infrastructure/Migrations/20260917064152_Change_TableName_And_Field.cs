573e4530555d695fe1fb5aa72299c4aeea8bee80
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-09-17 09:29:59 +0200

    Creada migracion de la correccion

---
+++ Bookify.Infrastructure/Migrations/20260917064152_Change_TableName_And_Field.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_TableName_And_Field : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_users_id",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "users");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                newName: "roles");
+
+            migrationBuilder.RenameTable(
+                name: "Reviews",
+                newName: "reviews");
+
+            migrationBuilder.RenameTable(
+                name: "Bookings",
+                newName: "bookings");
+
+            migrationBuilder.RenameTable(
+                name: "Apartments",
+                newName: "apartments");
+
+            migrationBuilder.RenameColumn(
+                name: "identity",
+                table: "users",
+                newName: "identity_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_identity_id",
+                table: "users",
+                column: "identity_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_users_identity_id",
+                table: "users");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "roles",
+                newName: "Roles");
+
+            migrationBuilder.RenameTable(
+                name: "reviews",
+                newName: "Reviews");
+
+            migrationBuilder.RenameTable(
+                name: "bookings",
+                newName: "Bookings");
+
+            migrationBuilder.RenameTable(
+                name: "apartments",
+                newName: "Apartments");
+
+            migrationBuilder.RenameColumn(
+                name: "identity_id",
+                table: "Users",
+                newName: "identity");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_id",
+                table: "Users",
+                column: "id",
+                unique: true);
+        }
+    }
+}
